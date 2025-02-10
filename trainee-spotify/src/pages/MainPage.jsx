ad3e3cc17de5426235562b79adb721a020ef682d
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-10 14:00:15 -0300

    Fix rendering issue

--- trainee-spotify/src/pages/MainPage.jsx
+++ trainee-spotify/src/pages/MainPage.jsx
@@ -179,6 +179,7 @@ function MainPage() {
                         <ArtistCard key={artist} artist={artist} />
                     ))}
                 </div>
+                )}
             </main>
         </div>
     );
