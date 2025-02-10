06831679b721791f21ff8980137da05a3f249dfc
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-10 13:59:31 -0300

    Add loading and error states

--- trainee-spotify/src/pages/MainPage.jsx
+++ trainee-spotify/src/pages/MainPage.jsx
@@ -169,6 +169,11 @@ function MainPage() {
             {/* Main content (scrollable) */}
             <main className="bg-[#101010] w-auto flex-grow overflow-y-auto h-screen p-0 pt-[4rem]">
                 <h2 className="hover:text-green-400 text-white text-center font-[500] pb-[1rem] sm:text-left sm:text-[2rem] pl-[2rem]">Artistas</h2>
+                {isLoading ? (
+                    <div className="text-white text-center mt-10">Carregando artistas...</div>
+                ) : error ? (
+                    <div className="text-red-500 text-center mt-10">{error}</div>
+                ) : (
                 <div className="grid grid-rows-auto justify-center gap-y-[2rem] gap-x-[0.2rem] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-[2rem]">
                     {allArtistsToBeDisplayed.map((artist) => (
                         <ArtistCard key={artist} artist={artist} />
