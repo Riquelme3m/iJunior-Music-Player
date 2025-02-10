a485e49c1ea4b864f8b00f0dea6a028488c32135
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-09 23:21:06 -0300

    Fix images sizing

--- trainee-spotify/src/components/ArtistCard.jsx
+++ trainee-spotify/src/components/ArtistCard.jsx
@@ -7,7 +7,7 @@ function ArtistCard( {artist}) {
   
   return (
     <div className="bg-[#181818] w-[180px] h-[250px] rounded-lg flex flex-col items-start  justify-around pb-[1rem] pt-[0.2rem]">
-      <img className="rounded-full w-[150px] m-auto " src={artist.image || dummyImage} alt={"Artist Picture"} />
+      <img className="rounded-full w-[150px] h-[150px] m-auto " src={artist.image || dummyImage} alt={"Artist Picture"} />
       <div className="flex flex-col items-start w-[100%]  pl-[1rem]  gap-[0.7rem]">
         <h4 className="text-white text-[0.9rem] font-[500]">{artist.name || "Banda do molejo"}</h4>
         <h4 className="text-[gray] font-[500] text-[0.9rem]">Artista</h4>
