7ad4da31a1b520d9bc1a018b6167eb49989a9040
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-13 03:22:35 -0300

    Set AppRouter and clean unused imports

--- trainee-spotify/src/App.jsx
+++ trainee-spotify/src/App.jsx
@@ -1,28 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import AppRouter from './routes/AppRouter'
-import LoginPage from './pages/LoginPage'
-
+import { checkLoginStatus } from './services/api'
 
 function App() {
-  
-
-  return (
-    // <AppRouter></AppRouter>
-    <LoginPage></LoginPage>
-    
-  )
+  return <AppRouter />
 }
 
 export default App
-
-/*
-  <i class="fa-solid fa-heart"></i>
-      <i class="fa-regular fa-registered"></i>
-      <i class="fa-solid fa-arrow-right-from-bracket"></i>
-      <span class="material-symbols-outlined">album</span>
-      /* boxes : width 196px height 280px*/ 
-    /*aside rectangle
-*/
