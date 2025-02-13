1160e7d1bef16c6592cdf242878bdaa60f70037f
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-13 03:48:16 -0300

    Setting pages routes based on logged in or not

--- trainee-spotify/src/routes/AppRouter.jsx
+++ trainee-spotify/src/routes/AppRouter.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { checkLoginStatus } from "../services/api";
 import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import CurtidasPage from "../pages/LikesPage";
 
 
-function AppRouter(){
+function AppRouter() {
+    const isLoggedIn = checkLoginStatus()
 
-    return(
+    return (
         <Router>
             <Routes>
-                <Route path="/" element={<MainPage></MainPage>}></Route>
+                // Login: caso ja estiver logado vai para /
+                <Route path="/login" element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />} />
+                <Route path="/signup" element={!isLoggedIn ? <SignupPage /> : <Navigate to="/" />} />
+
+                // Main: caso não estiver logado vai para /login
+                <Route path="/" element={
+                    isLoggedIn ? <MainPage /> : <Navigate to="/login" />
+                } />
+
+                // Likes: caso não estiver logado vai para /login
+                <Route path="/likes" element={
+                    isLoggedIn ? <CurtidasPage /> : <Navigate to="/login" />
+                } />
+
+                // Pagina não definida: vai para /
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
     )
