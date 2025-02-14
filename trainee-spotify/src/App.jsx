7adfab958ed6a3ef1a5c18ae352d412b8ab9ed21
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-13 22:34:41 -0300

    feat: remove unused imports

--- trainee-spotify/src/App.jsx
+++ trainee-spotify/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import AppRouter from './routes/AppRouter'
-import { checkLoginStatus } from './services/api'
 
 function App() {
   return <AppRouter />
