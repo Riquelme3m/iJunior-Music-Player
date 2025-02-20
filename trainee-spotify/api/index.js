99ab894bdcd3ff86c595ea784fb60e7cc4cffbb0
riquelme <riquelme3m@outlook.com>
2025-02-20 20:17:19 -0300

    changes for deploy

--- trainee-spotify/api/index.js
+++ trainee-spotify/api/index.js
@@ -1,3 +1,10 @@
 const app = require('./config/express-config');
 
+app.use("/",(req,res)=>{
+    res.send("Server is running.");
+});
+
 app.listen(3030, console.log('API listening on port 3030'));
+
+
+
