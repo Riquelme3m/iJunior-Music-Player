1fc384c92e7b057860148ffbbb80f61250d81a1b
riquelme <riquelme3m@outlook.com>
2025-02-20 20:07:09 -0300

    Trying to deploy again

--- trainee-spotify/api/index.js
+++ trainee-spotify/api/index.js
@@ -1,6 +1,3 @@
 const app = require('./config/express-config');
 
-app.use("/",(req,res)=>{
-    res.send("Server is running...");
-})
-app.listen(5000, console.log('API listening on port 5000'));
+app.listen(3030, console.log('API listening on port 3030'));
