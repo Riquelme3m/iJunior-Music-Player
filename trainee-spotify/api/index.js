465fd4407f86dff5f5f9b4251cfe5cb5ea76cef4
riquelme <riquelme3m@outlook.com>
2025-02-20 19:50:14 -0300

    more config files

--- trainee-spotify/api/index.js
+++ trainee-spotify/api/index.js
@@ -3,4 +3,4 @@ const app = require('./config/express-config');
 app.use("/",(req,res)=>{
     res.send("Server is running...");
 })
-app.listen(3030, console.log('API listening on port 3030'));
+app.listen(5000, console.log('API listening on port 5000'));
