9863b5c21bf9d7cd12c29d949ade2dfa894b6c1f
riquelme <riquelme3m@outlook.com>
2025-02-20 22:13:49 -0300

    Trying again

--- trainee-spotify/api/index.js
+++ trainee-spotify/api/index.js
@@ -1,3 +1,21 @@
 const app = require('./config/express-config');
 
+app.use("/",(req,res)=>{
+    res.send("Server is running");
+})
+
 app.listen(3030, console.log('API listening on port 3030'));
+
+
+
+//Code that runs on vercel okay 
+
+/*const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res)=>{
+    res.send("Server is running");
+});
+
+app.listen(3030,console.log("Server started on PORT 3030")); */
