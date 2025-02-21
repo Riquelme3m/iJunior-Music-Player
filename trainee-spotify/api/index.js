59abca8b0654cddc2bb8d56bc2481d7d105c8ff6
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-21 10:02:22 -0300

    Revert "last changes"
    This reverts commit babd21cb494e1f375cdb3d7e01f4f31632e8be36.

--- trainee-spotify/api/index.js
+++ trainee-spotify/api/index.js
@@ -1,21 +1,6 @@
 const app = require('./config/express-config');
 
 app.use("/",(req,res)=>{
-    res.send("Server is running");
+    res.send("Server is running...");
 })
-
 app.listen(3030, console.log('API listening on port 3030'));
-
-
-
-//Code that runs on vercel okay 
-
-/*const express = require("express");
-
-const app = express();
-
-app.use("/",(req,res)=>{
-    res.send("Server is running");
-});
-
-app.listen(3030,console.log("Server started on PORT 3030")); */
